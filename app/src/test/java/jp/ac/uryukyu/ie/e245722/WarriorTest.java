93baab4e653b6998209bf01c3c8ffbfa8c810bff
RIki Sakihara <e245722@ie.u-ryukyu.ac.jp>
2024-12-01 17:28:15 +0900

    4st commit

--- app/src/test/java/jp/ac/uryukyu/ie/e245722/WarriorTest.java
+++ app/src/test/java/jp/ac/uryukyu/ie/e245722/WarriorTest.java
@@ -4,22 +4,22 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class WarriorTest {
     /**
-     * 倒れたはずの敵は攻撃できないことを検証。
+     * ダメージが1.5倍かどうかを確認
      * 検証手順
-     *  (1) ヒーローと敵を準備。ヒーローの攻撃力は敵を一撃で倒せるほど強い状態とする。
-     *  (2) ヒーローが殴り、敵も殴る。敵は一撃で倒されていることを期待。
-     *  (3) 敵が死んだ状態ならば攻撃できないはず。
-     * 　　つまり攻撃実行してもヒーローのHPは減っていないことを期待。これを検証する。
+     *  (1) ヒーローと敵を準備。ヒーローの攻撃力の1.5倍の3倍の体力のスライムを用意。
+     *  (2) ヒーローが殴り、敵も殴る。敵にattackの1.5倍のダメージを与えていることを期待。
+     *  (3) きちんとスキルの与ダメが1.5倍されていれば、敵に与えたダメージ＝attack*1.5となる。
+     * 
      */
     @Test
     void attackTest() {
-        int defaultHeroHp = 100;
-        Warrior demoHero = new Warrior("デモ勇者", defaultHeroHp, 10);
-        Enemy slime = new Enemy("スライムもどき", 45, 100);
+        int defaultEnemyHp = 100;
+        Warrior demoHero = new Warrior("デモ勇者", 100, 10);
+        Enemy slime = new Enemy("スライムもどき", defaultEnemyHp, 100);
         for (int i = 0 ; i < 3 ; i++){
-        demoHero.attackWithWeponSkill(slime);
+            demoHero.attackWithWeponSkill(slime);
+            assertEquals(defaultEnemyHp - slime.getHitPoint(), (int)(demoHero.getAttak() * 1.5 * (i+1)) );
     }
         slime.attack(demoHero);
-        assertEquals(defaultHeroHp, demoHero.getHitPoint());
     }
 }
